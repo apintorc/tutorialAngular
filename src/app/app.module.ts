0e5549708b3ebe468e846f38a1d97cee6650fc1a
Pintor Correas external <alejandro.pintor-correas.external@capgemini.com>
2023-01-25 15:07:52 +0100

    ejercicio1.1: creacion gestion de clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { CoreModule } from './core/core.module';
 import { CategoryModule } from './category/category.module';
 import { AuthorModule } from './author/author.module';
 import { GameModule } from './game/game.module';
+import { ClientModule } from './client/client.module';
+
 
 @NgModule({
   declarations: [
@@ -20,6 +22,7 @@ import { GameModule } from './game/game.module';
     CategoryModule,
     AuthorModule,
     GameModule,
+    ClientModule,
     BrowserAnimationsModule
   ],
   providers: [],
---
+++ src/app/client/model/mock-clients.ts
@@ -0,0 +1,9 @@
+import { Client } from "./Client";
+
+export const CLIENT_DATA: Client[] = [
+    { id: 1, name: 'Alejandro' },
+    { id: 2, name: 'Pedro' },
+    { id: 3, name: 'Jaime' },
+    { id: 4, name: 'Cristina' },
+    { id: 5, name: 'Andrea' },
+]    
